11488c8b443525d6e76dabf9ebc32cbadab30595
Branislav Hozza <brankohozza@gmail.com>
2022-10-07 14:16:22 +0200

    :construction: fix: hide history when creating

--- client/src/views/EmployeeDetail.vue
+++ client/src/views/EmployeeDetail.vue
@@ -105,33 +105,35 @@ export default defineComponent({
 				}}
 				zamestnanca
 			</h1>
+			<div v-if="!creation">
+				<h1 class="text-xl m-2 font-semibold">Historia</h1>
+				<table>
+					<thead>
+						<tr>
+							<th class="border px-4 py-2">Pozicia</th>
+							<th class="border px-4 py-2">Od</th>
+							<th class="border px-4 py-2">Do</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="item in history" :key="item.id">
+							<td class="border px-4 py-2">
+								{{
+									positions.find(p => p.id == item.positionId)
+										?.name
+								}}
+							</td>
+							<td class="border px-4 py-2">
+								{{ item.startDate?.slice(0, 10) }}
+							</td>
+							<td class="border px-4 py-2">
+								{{ item.endDate?.slice(0, 10) }}
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 
-			<h1 class="text-xl m-2 font-semibold">Historia</h1>
-			<table>
-				<thead>
-					<tr>
-						<th class="border px-4 py-2">Pozicia</th>
-						<th class="border px-4 py-2">Od</th>
-						<th class="border px-4 py-2">Do</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr v-for="item in history" :key="item.id">
-						<td class="border px-4 py-2">
-							{{
-								positions.find(p => p.id == item.positionId)
-									?.name
-							}}
-						</td>
-						<td class="border px-4 py-2">
-							{{ item.startDate?.slice(0, 10) }}
-						</td>
-						<td class="border px-4 py-2">
-							{{ item.endDate?.slice(0, 10) }}
-						</td>
-					</tr>
-				</tbody>
-			</table>
 			<div class="w-full my-2">
 				<label
 					for="name"
